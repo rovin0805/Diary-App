6b3fe0a2a9de92fa0b25c3d5400a45d47d53a106
Choi Eun Jeong <qhdrn67@naver.com>
2021-11-28 02:16:13 +0900

    setting Realm

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigator from "./Navigator";
+import AppLoading from "expo-app-loading";
+import Realm from "realm";
+import { FeelingSchema } from "./schema";
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+
+  const startLoading = async () => {
+    const realm = await Realm.open({
+      path: "nomadDiaryDB",
+      schema: [FeelingSchema],
+    });
+  };
+
+  const onFinish = () => setReady(true);
+
+  if (!ready) {
+    return (
+      <AppLoading
+        onError={console.error}
+        startAsync={startLoading}
+        onFinish={onFinish}
+      />
+    );
+  }
+
   return (
     <NavigationContainer>
       <Navigator />
